8643a0db28fbf3ddb823cdc8aa71e3842a0d96d9
shincheolhwan <78342372+shincheolhwan@users.noreply.github.com>
2024-07-13 23:43:04 +0900

    [level 3] Title: 산 모양 타일링, Time: 70.97 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
@@ -1,5 +1,4 @@
 def solution(n, tops):
-    mod = 10007
     a = [0 for _ in range(n + 1)]
     b = [0 for _ in range(n + 1)]
     a[0] = 0
@@ -7,10 +6,11 @@ def solution(n, tops):
 
     for i in range(len(tops)):
         if tops[i] == 1:
-            a[i + 1] = (a[i] + b[i]) % mod
-            b[i + 1] = (2 * a[i] + 3 * b[i]) % mod
+            a[i + 1] = a[i] + b[i]
+            b[i + 1] = 2 * a[i] + 3 * b[i]
         else:
-            a[i + 1] = (a[i] + b[i]) % mod
-            b[i + 1] = (a[i] + 2 * b[i]) % mod
+            a[i + 1] = a[i] + b[i]
+            b[i + 1] = a[i] + 2 * b[i]
 
-    return (a[n] + b[n]) % mod
+    return (a[n] + b[n]) % 10007
+
